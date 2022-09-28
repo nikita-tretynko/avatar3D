371c4a6d520e475724c0260ef36da7b87a619315
nikita-tretynko <nikita.t@solid-sl.com>
2022-09-28 23:06:26 +0300

    Changed readme.me and added bootstrap to package.json

--- src/components/body-card.js
+++ src/components/body-card.js
@@ -6,7 +6,7 @@ const BodyCard = ({name, image, id, selectedBodyId, onSelectAvatarsBody}) => {
                 {selectedBodyId === id && <i className="fa-solid fa-circle-check fa-2x text-success selected-tick"></i>}
             </div>
             <div className="body-image">
-                <img src={image}/>
+                <img src={image} alt="Your or friend's selfie"/>
             </div>
         </div>
     )
